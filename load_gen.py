d919f1a4e77b6c594b3f8c69e1f0a899a2016b43
Anirudha Jitani <anirudhajitani@gmail.com>
2020-12-15 21:31:02 +0000

    code for restoring training and make sure exp varibale is not too high, logs supressed

--- load_gen.py
+++ load_gen.py
@@ -19,7 +19,7 @@ fc = 0
 def fireEvent(start_time):
     global fc
     x = randrange(0, 1)
-    print (x, time.time() - start_time)
+    #print (x, time.time() - start_time)
     q_str = 'http://' + ip_address + ':' + port + '?' + 'count=' + str(x)
     out = subprocess.Popen(['docker', 'run', '--rm', 'byrnedo/alpine-curl', '-s', q_str],
     #out = subprocess.Popen(['docker', 'run', '--rm', 'byrnedo/alpine-curl', '-w', '@curlformat', '-s', q_str],
@@ -29,8 +29,8 @@ def fireEvent(start_time):
     stdout, stderr = out.communicate()
     stdout = stdout.decode('utf-8')
     op = re.split('\[|, ', stdout)
-    # print(op)
-    print("Output ", op[1])
+    print(op)
+    #print("Output ", op[1])
     print(stdout)
     print(stderr)
     # Get new FC from stdout
@@ -41,15 +41,16 @@ def fireEvent(start_time):
 
 
 def run_rl_module_and_notify(fc, run):
+    print ("Notification module called")
+    q_str = 'http://' + ip_address + ':' + port + '/notify?' + 'offload=' + str(-run)
+    #out = subprocess.Popen(['docker', 'run', '--rm', 'curl_client', '-w', '@curlformat', '-s', q_str],
+    out = subprocess.Popen(['docker', 'run', '--rm', 'byrnedo/alpine-curl', '-s', q_str],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+    #print(stdout)
+    #print(stderr)
     if fc == 0:
-        q_str = 'http://' + ip_address + ':' + port + '/notify?' + 'offload=' + str(-run)
-        #out = subprocess.Popen(['docker', 'run', '--rm', 'curl_client', '-w', '@curlformat', '-s', q_str],
-        out = subprocess.Popen(['docker', 'run', '--rm', 'byrnedo/alpine-curl', '-s', q_str],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
-        stdout, stderr = out.communicate()
-        print(stdout)
-        print(stderr)
         return
 
     q_str = 'http://' + ip_address + ':' + port + '/notify?' + 'offload=' + str(fc)
@@ -58,8 +59,8 @@ def run_rl_module_and_notify(fc, run):
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT)
     stdout, stderr = out.communicate()
-    print(stdout)
-    print(stderr)
+    #print(stdout)
+    #print(stderr)
     files_dst = ['_ptr.npy', '_state.npy', '_next_state.npy',
                  '_action.npy', '_reward.npy', '_not_done.npy'] 
     files_dst_2 = ['overload_count.npy', 'offload_count.npy']
@@ -70,40 +71,41 @@ def run_rl_module_and_notify(fc, run):
                                stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT)
         stdout, stderr = out.communicate()
-        print(stdout)
-        print(stderr)
+        #print(stdout)
+        #print(stderr)
     for file_dst in files_dst_2:
         path_str = container_name + ':/buffer_' + str(run) + '_' + file_dst
         out = subprocess.Popen(['docker', 'cp', path_str, dest_path_str],
                                stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT)
         stdout, stderr = out.communicate()
-        print(stdout)
-        print(stderr)
+        #print(stdout)
+        #print(stderr)
     buffer_name = 'buffer_' + str(run) + '_' + str(fc)
     out = subprocess.Popen(['python3.7', 'main_train.py', '--replay_buffer', buffer_name, '--fc', str(fc), 
                             '--run', str(run), '--algo', '3', '--folder', folder, '--env_name', env_name],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT)
     stdout, stderr = out.communicate()
-    print(stdout)
-    print(stderr)
+    #print(stdout)
+    #print(stderr)
     dest_path_str = container_name + ':/req_thres.npy'
     src_path_str = f'./{folder}/buffers/thresvec_{str(run)}_{env_name}_{str(fc)}.npy'
     out = subprocess.Popen(['docker', 'cp', src_path_str, dest_path_str],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT)
     stdout, stderr = out.communicate()
-    print(stdout)
-    print(stderr)
+    #print(stdout)
+    #print(stderr)
     q_str = 'http://' + ip_address + ':' + port + '/notify?' + 'offload=0'
     #out = subprocess.Popen(['docker', 'run', '--rm', 'curl_client', '-w', '@curlformat', '-s', q_str],
     out = subprocess.Popen(['docker', 'run', '--rm', 'byrnedo/alpine-curl', '-s', q_str],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT)
     stdout, stderr = out.communicate()
-    print(stdout)
-    print(stderr)
+    #print(stdout)
+    #print(stderr)
+    print("NOTIF completed")
     return
 
 def process_event(lambd):
@@ -111,6 +113,7 @@ def process_event(lambd):
     while time.time() - start_time < 100:
         #interval = random.expovariate(0.1)
         interval = random.expovariate(lambd)
+        interval = min(interval, 20.0)
         time.sleep(interval)
         fireEvent(start_time)
 
@@ -123,17 +126,19 @@ def main():
         N = pickle.load(fp)
     for run in range(1,6):
         random.seed(run)
-        for l in range(1000):
+        for l in range(1, 1000):
+            print("RUN = ", run, " LOOP = ", l)
             jobs = []
             run_rl_module_and_notify(l, run)
             for i in range(N[l]):
-                print (lambd[l][i])
+                #print (lambd[l][i])
                 t = th.Thread(target=process_event, args=(lambd[l][i],))
                 jobs.append(t)
             for j in jobs:
                 j.start()
             for j in jobs:
                 j.join()
+            print("LOOP ", l, " complete ")
 
 
 if __name__ == "__main__":
